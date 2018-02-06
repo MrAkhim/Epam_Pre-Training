4c707ca38e0446c1c4123a969eb472081f7733bb
Ahim <mrkarpen@mail.ru>
2018-02-06 22:19:40 +0300

    Add files via upload

---
+++ Task2/4/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Test {
+public static void main(String[] args) {
+	int[] date=new int[] {25,3,1990} ;
+	DayPlusOne.nextDay(date[0], date[1], date[2]);
+
+	System.out.println(Arrays.toString(DayPlusOne.nextDay(date[0], date[1], date[2])));
+
+}
+}
