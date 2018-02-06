051e4752701a4c6141ae4b323f7e1d190bfd319c
Ahim <mrkarpen@mail.ru>
2018-02-06 22:20:18 +0300

    Update Test.java

--- Task2/4/Test.java
+++ Task2/4/Test.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	int[] date=new int[] {25,3,1990} ;
 	DayPlusOne.nextDay(date[0], date[1], date[2]);
 
-	System.out.println(Arrays.toString(DayPlusOne.nextDay(date[0], date[1], date[2])));
+//	System.out.println(Arrays.toString(DayPlusOne.nextDay(date[0], date[1], date[2])));
 
 }
 }
