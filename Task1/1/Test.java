b8e99f0d78af72a3a3fa4f3878c066307c8b1ded
Ahim <mrkarpen@mail.ru>
2018-02-19 22:41:27 +0300

    Add files via upload

---
+++ Task1/1/Test.java
@@ -0,0 +1,11 @@
+package by.epam.preTraining.task1.model;
+
+public class Test {
+	public static void main(String[] args) {
+		int gram=500;
+		Weight.convertGrtoKg(gram);
+		Weight.convertGrtoTon(gram);
+		
+		
+	}
+}
