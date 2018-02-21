c7c78da831ab805c82bcc95cdbe58f15ad7464bb
Ahim <mrkarpen@mail.ru>
2018-02-21 16:23:10 +0300

    Update Fibonacci.java

--- Task4/Model/Fibonacci.java
+++ Task4/Model/Fibonacci.java
@@ -2,11 +2,15 @@ package by.epam.preTraining.task4.model;
 
 public class Fibonacci {
 	public static int countFibonacciNumber(int index) {
-		if(index >1)
-			return index=countFibonacciNumber(index-1)+countFibonacciNumber(index-2);
-		if(index==1)
+		if (index > 1) {
+			return countFibonacciNumber(index - 1) + countFibonacciNumber(index - 2);
+		}
+		if (index == 1) {
 			return 1;
-		else 
-			return 0;
+		}
+		if (index < 0) {
+			throw new IllegalArgumentException("index must be positive:");
+		}
+		return 0;
 	}
 }
