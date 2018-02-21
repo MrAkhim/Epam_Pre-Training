1c701d52896dbe0e3aaecd81ed36915a45b3641a
Ahim <mrkarpen@mail.ru>
2018-02-21 13:24:23 +0300

    Update Sum.java

--- Task4/Model/Sum.java
+++ Task4/Model/Sum.java
@@ -1,10 +1,11 @@
 package by.epam.preTraining.task4.model;
 
 public class Sum {
-	public static int calculate_sum_of_digits(int number) {
-		if (number>0)
-		return number=number%10+calculate_sum_of_digits(number/10);
-		else
-			return 0;
+	public static int digitsSum(int number) {
+		number=Math.abs(number);
+		if (number > 0) {
+			return number % 10 + digitsSum(number / 10);
+		}
+		return 0;
 	}
 }
