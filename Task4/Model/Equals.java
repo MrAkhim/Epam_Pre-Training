9b428b224e73f5ecd0c32f6d36b70b2e5efbd62a
Ahim <mrkarpen@mail.ru>
2018-02-21 16:04:51 +0300

    Update Equals.java

--- Task4/Model/Equals.java
+++ Task4/Model/Equals.java
@@ -1,11 +1,10 @@
 package by.epam.preTraining.task4.model;
 
 public class Equals {
-	public static boolean check_sum_equivalent(int number,int s) {
-		boolean check=false;
-		if(Sum.calculate_sum_of_digits(number)==s)
-			check=true;
-		return check;
+	public static boolean checkSumEquivalent(int number, int sum) {
+		if (sum > 0 && number > 0) {
+			return checkSumEquivalent(number / 10, sum - number % 10);
+		}
+		return number == sum;
 	}
-
 }
