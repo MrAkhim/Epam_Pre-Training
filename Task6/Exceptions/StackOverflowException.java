22d4c3317c94d56d0491d131fed25c5ca779fd94
Ahim <mrkarpen@mail.ru>
2018-02-23 19:37:08 +0300

    Add files via upload

---
+++ Task6/Exceptions/StackOverflowException.java
@@ -0,0 +1,7 @@
+package by.epam.preTraining.task6.exceptions;
+
+public class StackOverflowException extends Exception {
+    public StackOverflowException(String msg) {
+        super(msg);
+    }
+}
