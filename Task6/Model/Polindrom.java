2c4675516561ac07a7db237dafb5e2504c9655bc
Ahim <mrkarpen@mail.ru>
2018-02-23 23:18:47 +0300

    Update Polindrom.java

--- Task6/Model/Polindrom.java
+++ Task6/Model/Polindrom.java
@@ -7,12 +7,12 @@ public class Polindrom {
 	public static boolean checkPalindrome(String str) throws StackOverflowException, StackEmptyException {
 		MyStackInterface<Character> string = new MyStackArray<>();
 		int halfString=str.length() / 2;
-		int i;
+		int i; //значение i будет использоваться и далее.
 		for (i = 0; i < halfString; i++) {
 			string.push(str.charAt(i));
 		}
-		if (str.length() % 2 == 1) {
-			i++;
+		if (str.length() % 2 == 1) { //если нечетная длинна - пропускаем центральный элемент
+			i++;			
 		}
 		while (!string.isEmpty()) {
 			if (string.pop() != str.charAt(i++)) {
