834ca34080fab594df66ae3e254c4c7556616f5e
Ahim <mrkarpen@mail.ru>
2018-02-24 15:14:15 +0300

    Update Polindrom.java

--- Task6/Model/Polindrom.java
+++ Task6/Model/Polindrom.java
@@ -6,19 +6,17 @@ import by.epam.preTraining.task6.exceptions.StackOverflowException;
 public class Polindrom {
 	public static boolean checkPalindrome(String str) throws StackOverflowException, StackEmptyException {
 		MyStackInterface<Character> string = new MyStackArray<>();
-		int halfString=str.length() / 2;
-		int i; //значение i будет использоваться и далее.
-		for (i = 0; i < halfString; i++) {
+		int half = str.length() / 2;
+		for (int i = half; i < str.length(); i++) {
 			string.push(str.charAt(i));
 		}
-		if (str.length() % 2 == 1) { //если нечетная длинна - пропускаем центральный элемент
-			i++;			
-		}
+		int i = 0;
 		while (!string.isEmpty()) {
 			if (string.pop() != str.charAt(i++)) {
 				return false;
-			}
+			}	
 		}
 		return true;
 	}
-}
+} 		 //Сложность О(N)
+
